4230d5de22df549ccedcfc396d874881812e8ef5
BeckaL <becka.lelew@digital.cabinet-office.gov.uk>
2024-11-08 05:09:08 +0000

    AUT-3820: Add decryption for jwt on authorize stub endpoint
    This involved:
    * Modifying the template.yml to ensure that the relevant environment parameters were picked up from parameters.json when running locally
    * Generating a key pair for use locally. These are committed here deliberately, as they are just for local testing use and will not be the same in any other environment
    * Using jose to decrypt the request
    * Adding a script to more easily generate the request parameter for local testing
    * Updating the readme

---
+++ ipv-stub/scripts/encrypt-message-locally.js
@@ -0,0 +1,33 @@
+const { CompactEncrypt, importSPKI } = require('jose');
+
+//This is the public key equivalent of the local private key in parameters.
+// Both have been committed deliberately to allow for local running and testing.
+const publicKeyPem = `-----BEGIN PUBLIC KEY-----
+      MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEApflLYqZm5IawLAHYtWoU
+  vKdO7cFBmDIOASlGgGCEG0PVBC4FJH2pM3FUw72n7YTS+H73Y8ZfTNIgu9K7zxEa
+  mCwimUAKU8Lsjq6Pqa0pZr2rE4l2MfO2j91uCcdlTzdM0kOkwcbzwqEdbDU+FJ4x
+  FT5aaOWyuN/BKFsc5kNz2t4+OaeRu/ev3h7WCqh2MMW5PWDbR2lBnKZR8HvuXZc5
+  ay0dUx098UjkLEBHIyT3FfzhXFMF2ZdOSysDMa64KwqeAWs6tjwM9+Bp3DYLkTsx
+  BML/eqgIwdZI5QBCTxD8YC2oxC1obMspiAoEz05wt8cYscmT4rZAdBGMspuxNqo4
+  6wIDAQAB
+  -----END PUBLIC KEY-----`;
+
+const algorithm = { alg: "some-alg" };
+const payload = { foo: "bar" }
+const signature = { sig: "a-signature" };
+
+const stringToEncrypt = [algorithm, payload, signature]
+    .map(e => JSON.stringify(e))
+    .map(element => Buffer.from(element.toString(), "utf-8").toString("base64"))
+    .join('.');
+
+const dataToEncrypt = Uint8Array.from(stringToEncrypt, c => c.charCodeAt(0));
+(async () => {
+    const publicKey = await importSPKI(publicKeyPem, "RSA-OAEP-256");
+
+    const jwe = await new CompactEncrypt(dataToEncrypt)
+        .setProtectedHeader({alg: "RSA-OAEP-256", enc: "A256GCM"})
+        .encrypt(publicKey);
+
+    console.log("Encrypted (JWE Compact):", jwe);
+})()
