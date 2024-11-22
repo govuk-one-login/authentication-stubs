35eeb39de44f9feda313a51d302032d4daf24752
Matt Votsikas McLean <matt.votsikas-mclean@digital.cabinet-office.gov.uk>
2024-11-22 11:44:00 +0000

    BAU: Retrieve access token in `/reverification`
    Using the access token passed in via the Authorisation header as a bearer token, look up the result from DynamoDB and return to the client.
    
    Bearer token is expected as https://github.com/govuk-one-login/authentication-api/commit/161a57673d929a2348c464b884ccf6d5b707efe9 implements sending it in the `ReverificationResultsHandler`.
    
    If the access token is missing, return an `invalid_token` error in accordance with RFC6750 "OAuth 2.0 Bearer Token Usage" Section 3.1: https://www.rfc-editor.org/rfc/rfc6750.txt (according to spec we should not present the reason if it's missing however this same reason will be used for when no result is recorded with the presented token as a shortcut for implementing a full OIDC server for the stub).

--- ipv-stub/src/endpoints/ipv-reverification.ts
+++ ipv-stub/src/endpoints/ipv-reverification.ts
@@ -5,9 +5,11 @@ import {
 } from "aws-lambda";
 import {
   handleErrors,
+  invalidAccessTokenResult,
   methodNotAllowedError,
   successfulJsonResult,
 } from "../helper/result-helper";
+import { logger } from "../helper/logger";
 
 export const handler: Handler = async (
   event: APIGatewayProxyEvent
@@ -23,10 +25,24 @@ export const handler: Handler = async (
 };
 
 async function get(
-  _event: APIGatewayProxyEvent
+  event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> {
+  const accessToken = getAccessToken(event);
+  if (!accessToken) {
+    logger.info("No access token found in event");
+    return invalidAccessTokenResult();
+  }
+
   return successfulJsonResult(200, {
     sub: "urn:fdc:gov.uk:2022:fake_common_subject_identifier",
     success: true,
   });
+}
+
+function getAccessToken(event: APIGatewayProxyEvent): string | undefined {
+  const authorization = event.headers["Authorization"];
+  if (!authorization || !authorization.startsWith("Bearer ")) {
+    return undefined;
+  }
+  return authorization.replace("Bearer ", "");
 }
