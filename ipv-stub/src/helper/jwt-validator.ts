2450265e203c9ef858b578fcf7554f34006c35a7
ChristianSoftwire <145482922+ChristianSoftwire@users.noreply.github.com>
2025-11-27 15:36:06 +0000

    Revert "Aut 4649 call correct auth api jwks for all jwt validation fix"

--- ipv-stub/src/helper/jwt-validator.ts
+++ ipv-stub/src/helper/jwt-validator.ts
@@ -3,23 +3,23 @@ import {
   DecodedStorageAccessToken,
   EncodedUserInfoClaim,
 } from "./types";
-import { compactVerify, KeyLike } from "jose";
+import {
+  KeyLike,
+  compactVerify,
+  importJWK,
+  importSPKI,
+  decodeProtectedHeader,
+} from "jose";
+import { CodedError } from "./result-helper";
 import process from "node:process";
 import { processJoseError } from "./error-helper";
-import { getPublicSigningKey } from "./jwks-helper";
+import { logger } from "./logger";
 
 export async function validateAuthorisationJwt(
   nestedJws: string
 ): Promise<DecodedRequest | string> {
-  const publicJwk = await getPublicSigningKey(
-    nestedJws,
-    process.env.AUTH_IPV_PUBLIC_SIGNING_KEY_JWKS_ENDPOINT,
-    process.env.AUTH_PUBLIC_SIGNING_KEY_IPV
-  );
-  const authoriseRequestAsJson = await verifyAndDecodeJwt<DecodedRequest>(
-    nestedJws,
-    publicJwk
-  );
+  const publicJwk = await getPublicSigningKey(nestedJws);
+  const authoriseRequestAsJson = await verifyAndDecodeJwt(nestedJws, publicJwk);
 
   const validationError = validatePayloadFields(authoriseRequestAsJson);
   if (validationError) return validationError;
@@ -27,13 +27,46 @@ export async function validateAuthorisationJwt(
   return await processStorageAccessToken(authoriseRequestAsJson);
 }
 
-async function verifyAndDecodeJwt<T>(
-  jws: string,
+async function getPublicSigningKey(nestedJws: string): Promise<KeyLike> {
+  const header = decodeProtectedHeader(nestedJws);
+  const kid = header.kid;
+
+  if (kid) {
+    logger.info("kid received in decoded protected header");
+    return await getPublicKeyFromJwks(kid);
+  }
+
+  const authSignaturePublicKeyIpv = process.env.AUTH_PUBLIC_SIGNING_KEY_IPV;
+  if (!authSignaturePublicKeyIpv) {
+    throw new CodedError(500, "Auth IPV signing public key not found");
+  }
+  return await importSPKI(authSignaturePublicKeyIpv, "ES256");
+}
+
+async function getPublicKeyFromJwks(kid: string): Promise<KeyLike> {
+  const jwksUri = process.env.AUTH_IPV_PUBLIC_SIGNING_KEY_JWKS_ENDPOINT;
+  if (!jwksUri) {
+    throw new CodedError(500, "JWKS URI not found");
+  }
+
+  const jwks = await fetchJwks(jwksUri);
+  for (const k of jwks.keys) {
+    if (k.kid === kid) {
+      logger.info(`using kid: ${kid}`);
+      return (await importJWK(k)) as KeyLike;
+    }
+  }
+
+  throw new CodedError(400, "Key not found in JWKS for provided kid");
+}
+
+async function verifyAndDecodeJwt(
+  nestedJws: string,
   publicJwk: KeyLike
-): Promise<T> {
+): Promise<DecodedRequest> {
   let payload;
   try {
-    ({ payload } = await compactVerify(jws, publicJwk));
+    ({ payload } = await compactVerify(nestedJws, publicJwk));
   } catch (error) {
     processJoseError(error);
   }
@@ -83,6 +116,14 @@ async function processStorageAccessToken(
   return authoriseRequestAsJson;
 }
 
+async function fetchJwks(jwksUri: string) {
+  const response = await fetch(jwksUri);
+  if (!response.ok) {
+    throw new CodedError(500, `Failed to fetch JWKS: ${response.statusText}`);
+  }
+  return await response.json();
+}
+
 async function validateStorageAccessTokenJWT(
   userInfo: EncodedUserInfoClaim
 ): Promise<DecodedStorageAccessToken | string> {
@@ -90,26 +131,33 @@ async function validateStorageAccessTokenJWT(
     userInfo["https://vocab.account.gov.uk/v1/storageAccessToken"]?.values !=
     undefined;
 
+  let payload;
+  let decodedPayloadAsJson;
+
   if (hasAccessTokenValues) {
     const storageTokenJws =
       userInfo["https://vocab.account.gov.uk/v1/storageAccessToken"].values[0];
 
-    const authSignaturePublicKey = await getPublicSigningKey(
-      storageTokenJws,
-      process.env.AUTH_IPV_STORAGE_TOKEN_SIGNING_KEY_JWKS_ENDPOINT,
-      process.env.AUTH_PUBLIC_SIGNING_KEY_EVCS
-    );
-    const decodedPayloadAsJson =
-      await verifyAndDecodeJwt<DecodedStorageAccessToken>(
-        storageTokenJws,
-        authSignaturePublicKey
-      );
+    const authSignaturePublicKey = process.env.AUTH_PUBLIC_SIGNING_KEY_EVCS;
+    if (!authSignaturePublicKey) {
+      throw new CodedError(500, "Auth EVCS signing public key not found");
+    }
+
+    const publicJwk = await importSPKI(authSignaturePublicKey, "ES256");
+    try {
+      ({ payload } = await compactVerify(storageTokenJws, publicJwk));
+      const textDecoder = new TextDecoder();
+      decodedPayloadAsJson = JSON.parse(textDecoder.decode(payload));
+    } catch (error) {
+      processJoseError(error);
+    }
 
     try {
       if (decodedPayloadAsJson.scope !== "reverification") {
         return "Storage access token scope is not reverification";
       }
-      return decodedPayloadAsJson;
+
+      return decodedPayloadAsJson as DecodedStorageAccessToken;
     } catch (_e) {
       return "Storage access token payload is not valid json";
     }
