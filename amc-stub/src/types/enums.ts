bd5dc5a08010b23c8cedc8eaf0fee8188261c0df
Aidan Comer <alhcomer@hotmail.co.uk>
2025-12-15 13:37:46 +0000

    AUT-4004: Crete script to create JWT for local testing
    - Script to be used to test functionality of amc-authorize.ts
    - Contains access token with ES256 signature
    - That access token is package in transporter JWT signed using
      ES256 algorith, encrypted using RSA_OAEP_256
    - Claims in this JWT will be validated in the stub authorize handler

--- amc-stub/src/types/enums.ts
+++ amc-stub/src/types/enums.ts
@@ -10,3 +10,13 @@ export enum AMCStubEndpoints {
   TOKEN = "/token",
   JOURNEY_OUTCOME = "/journeyoutcome",
 }
+
+export enum JoseAlgorithms {
+  ES256 = "ES256",
+  RSA_OAEP_256 = "RSA-OAEP-256",
+  A256GCM = "A256GCM",
+}
+
+export enum AMCScopes {
+  ACCOUNT_DELETE = "account-delete",
+}
