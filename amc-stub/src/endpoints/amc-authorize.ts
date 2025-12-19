dada12e6576466aceb4d709059f9ff68d5428681
Aidan Comer <alhcomer@hotmail.co.uk>
2025-12-19 15:16:40 +0000

    AUT-4004: Basic AMC Authorize Changes

--- amc-stub/src/endpoints/amc-authorize.ts
+++ amc-stub/src/endpoints/amc-authorize.ts
@@ -2,6 +2,9 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { logger } from "../../logger.js";
 import { CodedError, successfulJsonResult } from "../helpers/result-helper.js";
 import { HttpMethod } from "../types/enums.js";
+import { compactDecrypt, importPKCS8 } from "jose";
+import { processJoseError } from "../helpers/error-helper.ts";
+import { validateCompositeJWT } from "../helpers/jwt-validator.ts";
 
 export const handler = async (
   event: APIGatewayProxyEvent
@@ -16,7 +19,40 @@ export const handler = async (
   }
 };
 
-function get(_event: APIGatewayProxyEvent) {
+async function get(event: APIGatewayProxyEvent) {
+  logger.info("IPV Authorize GET endpoint invoked!");
+
+  if (event.queryStringParameters == null) {
+    throw new CodedError(400, "Query string parameters are null");
+  }
+
+  const encryptedJwt = event.queryStringParameters["request"] as string;
+  if (!encryptedJwt) {
+    throw new CodedError(400, "Request query string parameter not found");
+  }
+
+  const amcPrivateEncryptionKey = process.env.AMC_PRIVATE_ENCRYPTION_KEY;
+  if (!amcPrivateEncryptionKey) {
+    throw new CodedError(500, "Private encryption key not found");
+  }
+  const privateKey = await importPKCS8(amcPrivateEncryptionKey, "RSA-OAEP-256");
+
+  let plaintext, protectedHeader;
+  try {
+    ({ plaintext, protectedHeader } = await compactDecrypt(
+      encryptedJwt,
+      privateKey
+    ));
+  } catch (error) {
+    processJoseError(error);
+  }
+
+  if (plaintext === undefined || protectedHeader === undefined) {
+    throw new CodedError(500, "compactDecrypt returned undefined values");
+  }
+
+  const encodedJwt = plaintext.toString();
+
   return successfulJsonResult(200, { message: "Great success" });
 }
 
